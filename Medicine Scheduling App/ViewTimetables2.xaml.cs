fa3de264b5744b552bd0c10d951af89caeced156
lynseylyit <l00150013@student.lyit.ie>
2018-12-16 21:08:06 +0000

    Layout in place

---
+++ Medicine Scheduling App/ViewTimetables2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Medicine_Scheduling_App
+{
+    /// <summary>
+    /// Interaction logic for ViewTimetables2xaml.xaml
+    /// </summary>
+    public partial class ViewTimetables2 : Window
+    {
+        public ViewTimetables2()
+        {
+            InitializeComponent();
+        }
+    }
+}
